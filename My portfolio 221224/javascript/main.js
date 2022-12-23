95ceee3417973bbc586e6195b587681ec89fa656
nanopapa <88864315+nanopapa@users.noreply.github.com>
2022-12-24 07:16:26 +0900

    Add files via upload

---
+++ My portfolio 221224/javascript/main.js
@@ -0,0 +1,54 @@
+'use strict';
+{
+// FadeIn
+
+const targets = document.querySelectorAll('.fadein');
+
+function callback(entries,obs){
+    console.log(entries);
+
+entries.forEach(entry =>{
+    if(!entry.isIntersecting){
+        return;
+    }
+
+    entry.target.classList.add('appear');
+    obs.unobserve(entry.target);
+ 
+});
+
+}
+
+const options = {
+    threshold:0.6,
+
+};
+
+const observer = new IntersectionObserver(callback,options);
+
+    targets.forEach(target =>{
+        observer.observe(target);
+
+    });
+// FadeIn end
+
+
+}
+
+
+{
+const open=  document.getElementById('open');
+const sp__menu =document.querySelector('.sp__menu');
+
+open.addEventListener('click',() =>{
+open.classList.toggle('show');
+sp__menu.classList.toggle('panelactive');
+
+});
+
+sp__menu.addEventListener('click',() =>{
+    open.classList.remove('show');
+    sp__menu.classList.toggle('panelactive');
+});
+}
+// end 
